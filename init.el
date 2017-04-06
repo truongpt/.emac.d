f6cb4cc0c6980110db17b444b00177479f7b3d5b
Pham Trong Truong <truongptk30a3@gmail.com>
2017-04-06 15:16:35 +0900

    add copy function

--- init.el
+++ init.el
@@ -50,5 +50,96 @@
 (global-set-key (kbd "C-<up>")    'windmove-up)
 (global-set-key (kbd "C-<down>")  'windmove-down)
 
-
 (global-set-key "\C-q" 'toggle-truncate-lines)
+
+;;copy word
+(defun copy-word (&optional arg)
+ "Copy words at point into kill-ring"
+  (interactive "P")
+  (copy-thing 'backward-word 'forward-word arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key (kbd "C-c o") 'copy-word)
+
+;;copy line
+(defun copy-line (&optional arg)
+  "Save current line into Kill-Ring without mark the line "
+  (interactive "P")
+  (copy-thing 'beginning-of-line 'end-of-line arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key (kbd "C-c l") 'copy-line)
+
+;;copy paragraph
+(defun copy-paragraph (&optional arg)
+  "Copy paragraphes at point"
+  (interactive "P")
+  (copy-thing 'backward-paragraph 'forward-paragraph arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key (kbd "C-c p") 'copy-paragraph)
+
+;; copy string
+(defun beginning-of-string(&optional arg)
+  "  "
+  (re-search-backward "[ \t]" (line-beginning-position) 3 1)
+  (if (looking-at "[\t ]")  (goto-char (+ (point) 1)) )
+  )
+
+(defun end-of-string(&optional arg)
+  " "
+  (re-search-forward "[ \t]" (line-end-position) 3 arg)
+  (if (looking-back "[\t ]") (goto-char (- (point) 1)) )
+  )
+
+(defun thing-copy-string-to-mark(&optional arg)
+  " Try to copy a string and paste it to the mark
+     When used in shell-mode, it will paste string on shell prompt by default "
+  (interactive "P")
+  (copy-thing 'beginning-of-string 'end-of-string arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key (kbd "C-c s") 'thing-copy-string-to-mark)
+
+;;copy parenthesis
+(defun beginning-of-parenthesis(&optional arg)
+  "  "
+  (re-search-backward "[[<(?\"]" (line-beginning-position) 3 1)
+  (if (looking-at "[[<(?\"]")  (goto-char (+ (point) 1)) )
+  )
+(defun end-of-parenthesis(&optional arg)
+  " "
+  (re-search-forward "[]>)?\"]" (line-end-position) 3 arg)
+  (if (looking-back "[]>)?\"]") (goto-char (- (point) 1)) )
+  )
+
+(defun thing-copy-parenthesis-to-mark(&optional arg)
+  " Try to copy a parenthesis and paste it to the mark
+     When used in shell-mode, it will paste parenthesis on shell prompt by default "
+  (interactive "P")
+  (copy-thing 'beginning-of-parenthesis 'end-of-parenthesis arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key (kbd "C-c a") 'thing-copy-parenthesis-to-mark)
+
+;; copy group word
+(defun beginning-of-group_word(&optional arg)
+  "  "
+  (re-search-backward "[ )(]" (line-beginning-position) 3 1)
+  (if (looking-at "[)( ]")  (goto-char (+ (point) 1)) )
+  )
+
+(defun end-of-group_word(&optional arg)
+  " "
+  (re-search-forward "[ )(]" (line-end-position) 3 arg)
+  (if (looking-back "[)( ]") (goto-char (- (point) 1)) )
+  )
+
+(defun thing-copy-group_word(&optional arg)
+  " Try to copy a string and paste it to the mark
+     When used in shell-mode, it will paste string on shell prompt by default "
+  (interactive "P")
+  (copy-thing 'beginning-of-group_word 'end-of-group_word arg)
+  ;;(paste-to-mark arg)
+  )
+(global-set-key (kbd "C-c w") 'thing-copy-group_word)
