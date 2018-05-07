c12d0f52c3204f4831a1d546d367597e3abefc69
truongpt <truongptk30a3@gmail.com>
2018-05-07 22:23:28 +0900

    change default font to Consolas

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 ;;(setq url-proxy-services '(("http" . "pac.mei.co.jp/proxy.pac:80")))
 
 ;;(load-file "~/.emacs.d/elpa/setup-cygwin.el")
-;;(load-file "~/.emacs.d/elpa/gtags-el/gtags.el")
+(load-file "~/.emacs.d/elpa/gtags-el/gtags.el")
 (load-file "~/.emacs.d/elpa/csharp-mode/csharp-mode.el")
 ;;(require 'csharp-mode)
 ;;(csharp-mode 1)
@@ -32,13 +32,14 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
+ '(cua-mode t nil (cua-base))
  '(custom-enabled-themes (quote (wheatgrass))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
 
 (global-set-key "\C-f" 'compile)
 (global-set-key "\C-h" 'dired-up-directory)
