9e32a9f84e841f8d37e3de44f56161a71990ce92
truongpt <truongptk30a3@gmail.com>
2018-06-28 10:13:51 +0900

    tentative disable ivy & helm

--- init.el
+++ init.el
@@ -43,6 +43,10 @@
 ;; High light line
 (global-hl-line-mode 1)
 
+;; Display paren (highlight matching brackets)
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+
 ;; Gtags
 (load-file "~/.emacs.d/elpa/gtags-el/gtags.el")
 (require 'gtags)
@@ -259,71 +263,58 @@
 ;;(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 ;;(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (helm with-editor popup counsel))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-
-(require 'helm)
-(require 'helm-config)
-
-;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
-;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
-;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-unset-key (kbd "C-x c"))
-
-(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
-(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
-(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
-
-(when (executable-find "curl")
-  (setq helm-google-suggest-use-curl-p t))
-
-(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
-      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
-      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
-      helm-ff-file-name-history-use-recentf t
-      helm-echo-input-in-header-line t)
-
-(defun spacemacs//helm-hide-minibuffer-maybe ()
-  "Hide minibuffer in Helm session if we use the header line as input field."
-  (when (with-helm-buffer helm-echo-input-in-header-line)
-    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-      (overlay-put ov 'window (selected-window))
-      (overlay-put ov 'face
-                   (let ((bg-color (face-background 'default nil)))
-                     `(:background ,bg-color :foreground ,bg-color)))
-      (setq-local cursor-type nil))))
-
-
-(add-hook 'helm-minibuffer-set-up-hook
-          'spacemacs//helm-hide-minibuffer-maybe)
-
-(setq helm-autoresize-max-height 0)
-(setq helm-autoresize-min-height 20)
-(helm-autoresize-mode 1)
-
-(helm-mode 1)
-
-(global-set-key (kbd "M-x") 'helm-M-x)
-(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x b") 'helm-mini)
-(setq helm-buffers-fuzzy-matching t
-      helm-recentf-fuzzy-match    t)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-(when (executable-find "ack-grep")
-  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
-        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
+;;(require 'helm)
+;;(require 'helm-config)
+;; 
+;;;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;;;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;;;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+;;(global-set-key (kbd "C-c h") 'helm-command-prefix)
+;;(global-unset-key (kbd "C-x c"))
+;; 
+;;(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+;;(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+;;(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+;; 
+;;(when (executable-find "curl")
+;;  (setq helm-google-suggest-use-curl-p t))
+;; 
+;;(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+;;      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+;;      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+;;      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+;;      helm-ff-file-name-history-use-recentf t
+;;      helm-echo-input-in-header-line t)
+;; 
+;;(defun spacemacs//helm-hide-minibuffer-maybe ()
+;;  "Hide minibuffer in Helm session if we use the header line as input field."
+;;  (when (with-helm-buffer helm-echo-input-in-header-line)
+;;    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+;;      (overlay-put ov 'window (selected-window))
+;;      (overlay-put ov 'face
+;;                   (let ((bg-color (face-background 'default nil)))
+;;                     `(:background ,bg-color :foreground ,bg-color)))
+;;      (setq-local cursor-type nil))))
+;; 
+;; 
+;;(add-hook 'helm-minibuffer-set-up-hook
+;;          'spacemacs//helm-hide-minibuffer-maybe)
+;; 
+;;(setq helm-autoresize-max-height 0)
+;;(setq helm-autoresize-min-height 20)
+;;(helm-autoresize-mode 1)
+;; 
+;;;;(helm-mode 1)
+;; 
+;;(global-set-key (kbd "M-x") 'helm-M-x)
+;;(setq helm-M-x-fuzzy-match t) ;; optional fuzzy matching for helm-M
+;;(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+;;(global-set-key (kbd "C-x b") 'helm-mini)
+;;(setq helm-buffers-fuzzy-matching t
+;;      helm-recentf-fuzzy-match    t)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;;(global-set-key (kbd "M-s") 'helm-occur)
+;; 
+;;(when (executable-find "ack-grep")
+;;  (setq helm-grep-default-command "ack-grep -Hn --no-group --no-color %e %p %f"
+;;        helm-grep-default-recurse-command "ack-grep -H --no-group --no-color %e %p %f"))
